103bbc1fee710e9e8a1579d4f4f875713247da90
Macmiller Duarte de Andrade <macamagolf@gmail.com>
2025-09-09 20:30:17 -0300

    Feat: Tests userController updateUser (#13)

---
+++ src/utils/FetchFunctionsTests/patchFunction.js
@@ -0,0 +1,19 @@
+const host = process.env.HOST;
+const port = process.env.TESTS_PORT;          
+
+export async function makePATCHRequests(endPoint, data) {
+    
+    const request = await fetch(`http://${host}:${port}/${endPoint}/${data.user_id}`, {
+        method: "PATCH",
+        body: JSON.stringify({ username: data.username }),
+        headers: {
+            Authorization: `Bearer ${data.token}`
+        }, 
+
+    }).then(async (response) => {
+        const resp = await new Promise(resolve => resolve(response.json()));
+        return resp;  
+    });
+
+    return request;
+};
