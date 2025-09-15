914ee3052a93366305eb1cff73842b69abfa55ca
macmiller87 <macamagolf@gmail.com>
2025-09-15 17:42:20 -0300

    Feat: listProduct and updateProduct routes

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,13 +1,16 @@
+import { ProductsModelRepository } from "../models/productsModel.js";
 import { ProductsService } from "../services/productsService.js";
 import { Middleware } from "../utils/auth/middleware.js";
 import { decode } from "jsonwebtoken"
 import { once } from "node:events";
 
 export class ProductsController {
+    _productsModelRepository;
     _productsService;
     _userMiddleware;
 
-    constructor(productsService = new ProductsService, userMiddleware = new Middleware()) {
+    constructor(productsService = new ProductsService, userMiddleware = new Middleware(), productsModelRepository = new ProductsModelRepository()) {
+        this._productsModelRepository = productsModelRepository;
         this._productsService = productsService;
         this._userMiddleware = userMiddleware;
     }
@@ -52,6 +55,97 @@ export class ProductsController {
                     price: createProduct.price,
                     category: createProduct.category,
                     createdAt: createProduct.createdAt
+                }
+            }));
+
+        }
+
+        response.writeHead(401);
+        return response.end(JSON.stringify({ message: checkToken }));
+    }
+
+    async listProduct(request, response) {
+
+        const checkToken = await this._userMiddleware.ensureUserAuthenthicated(request, response);
+
+        if(checkToken === true) {
+
+            const url = request.url;
+            const splitUrl = url.split("/");
+            const productId = splitUrl[2];
+
+            if(productId === "") {
+                response.writeHead(401);
+                return response.end(JSON.stringify({ message: "Product ID must have a value !" }));
+            }
+
+            const findProductById = await this._productsService.listProduct(productId);
+
+            if(!findProductById) {
+                response.writeHead(404);
+                return response.end(JSON.stringify({ message: "Product not found !" }));
+            }
+
+            response.writeHead(200);
+            return response.end(JSON.stringify({
+                product: {
+                    product_id: findProductById.product_id,
+                    product_name: findProductById.product_name,
+                    price: findProductById.price,
+                    category: findProductById.category,
+                    createdAt: findProductById.createdAt
+                }
+            }));
+
+        }
+
+        response.writeHead(401);
+        return response.end(JSON.stringify({ message: checkToken }));
+    }
+
+    async updateProduct(request, response) {
+
+        const checkToken = await this._userMiddleware.ensureUserAuthenthicated(request, response);
+
+        if(checkToken === true) {
+
+            const url = request.url;
+            const splitUrl = url.split("/");
+            const product_id = splitUrl[2];
+
+            if(product_id === "") {
+                response.writeHead(401);
+                return response.end(JSON.stringify({ message: "Product ID must have a value !" }));
+            }
+
+            const { price } = JSON.parse(await once(request, "data"));
+
+            if(price === "") {
+                response.writeHead(401);
+                return response.end(JSON.stringify({ message: "Price must have a value !" }));
+
+            }else if(typeof(price) !== "number") {
+                response.writeHead(401);
+                return response.end(JSON.stringify({ message: "Price must be a float/decimal number !" }));
+            }
+
+            const findProductById = await this._productsModelRepository.findProductById(product_id);
+
+            if(!findProductById) {
+                response.writeHead(404);
+                return response.end(JSON.stringify({ message: "User not found !" }));
+            }
+
+            const updateProduct = await this._productsService.updateProduct(product_id, price);
+
+            response.writeHead(201);
+            return response.end(JSON.stringify({
+                product: {
+                    product_id: updateProduct.product_id,
+                    product_name: updateProduct.product_name,
+                    price: updateProduct.price,
+                    category: updateProduct.category,
+                    createdAt: updateProduct.createdAt
                 }
             }));
 
--- src/services/productsService.js
+++ src/services/productsService.js
@@ -12,4 +12,14 @@ export class ProductsService {
         return create;
     }
 
+    async listProduct(product_id) {
+        const find =  await this._productsModelRepository.listProduct(product_id);
+        return find;
+    }
+
+    async updateProduct(product_id, price) {
+        const update = await this._productsModelRepository.updateProduct(product_id, price);
+        return update;
+    }
+
 }
